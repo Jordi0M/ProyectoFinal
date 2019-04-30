141fb610c5c3aaba58464d1efa64ebecc7b2f892
jordi <jmartinezmateos@iesesteveterradas.cat>
2019-04-30 16:56:58 +0200

    comenzando

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.master')
 
 @section('contenido')
-    
+    <div class="row">
+        <div class="col-sm-2">Aqui va El Logo aaaaaaaaaaaaaaaaaaaa</div>
+        <div class="col-sm-10">AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            AAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        </div>
+    </div>
 @endsection
