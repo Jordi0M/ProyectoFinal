81d0b56454b6a13d6b655b0baf6f31cbcbc94aec
jordi0m <jmartinezmateos@iesesteveterradas.cat>
2019-04-30 12:50:36 +0200

    ruta, controlador, modelo y vistas para hacer

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Panel</title>
+</head>
+<body>
+    @yield('contenido')
+</body>
+</html>
