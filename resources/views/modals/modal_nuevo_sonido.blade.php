a5b830035d150c797ec5dd31259fd0fe0fb6c216
jordi0m <jmartinezmateos@iesesteveterradas.cat>
2019-05-04 18:18:36 +0200

    pequeño cambio al seleccionar el fichero a subir

--- resources/views/modals/modal_nuevo_sonido.blade.php
+++ resources/views/modals/modal_nuevo_sonido.blade.php
@@ -33,7 +33,7 @@
                             <div class="form-group" id="div_subir_sonido">
                                 <h3>Introduce el sonido</h3>
                                 <br>
-                                <input type="file" name="sonido" id="subida_nuevo_sonido">
+                                <input type="file" name="sonido" id="subida_nuevo_sonido" accept="audio/mp3, video/mp4, audio/wav, audio/mid">
                             </div>
                         </div>
                         <div class="modal-footer">
