8064a0d47a49613c9e94fcfcf1d92dc143710c94
jordi0m <jmartinezmateos@iesesteveterradas.cat>
2019-05-10 22:54:25 +0200

    avances logout y login localstorage

--- resources/views/destruir_sesion.blade.php
+++ resources/views/destruir_sesion.blade.php
@@ -1,4 +1,17 @@
 <script>
+
     localStorage.clear();
     window.location = "/";
+
+    ////////////LOGOUT, se guarda las iniciales.
+    ///no sirve en esta blade, porque esta es para el login (lo de arriba);
+    /*
+    console.log(JSON.parse(localStorage.local_tracks));
+    var tracks_iniciales = [];
+    for (let index = 0; index < 6; index++) {
+        console.log(tracks_iniciales);
+        tracks_iniciales.push(JSON.parse(localStorage.local_tracks)[index]);
+    }
+    console.log(tracks_iniciales);
+    */
 </script>
